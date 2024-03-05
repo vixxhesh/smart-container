3e9621648bff0a292b0cb87d3254d340edebbb59
vishesh <vishesh.verma1@s.amity.edu>
2024-03-05 21:14:34 +0530

    Added load to query

--- post-data.php
+++ post-data.php
@@ -5,22 +5,23 @@ $username = "id21696519_container";
 $password = "C0nt@iner";
 
 $api_key_value = "12345";
-$api_key = $gas_reading = $container_closed = $weight_read  = $value3 = "";
+$api_key = $gas_reading = $container_closed = $Wload = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
     if ($api_key == $api_key_value) {
         $gas_reading = test_input($_POST["gas_reading"]);
-        $container_closed = test_input($_POST["checkContainer"]);
-        // $timestamp = test_input($_POST["timestamp"]); // Add timestamp
+        // Check if the container is closed (received as boolean)
+        $container_closed = test_input($_POST["container_closed"]);
+        $Wload = test_input($_POST["Wload"]);
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "INSERT INTO sensor_data (gas_reading, container_closed)
-        VALUES ('" . $gas_reading . "', '" . $container_closed . "')"; // Adjust SQL query
+        $sql = "INSERT INTO sensor_data (gas_reading, container_closed, Wload)
+        VALUES ('" . $gas_reading . "', '" . $container_closed . "', '" . $Wload . "')"; // Adjust SQL query
 
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
